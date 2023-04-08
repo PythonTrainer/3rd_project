8c1c612490064c9d5fbd249c56071c25c4473feb
PythonTrainer <127989964+PythonTrainer@users.noreply.github.com>
2023-04-08 18:33:39 +0300

    Добавлен блок ifmain. Основная программа обернута функцией main()

--- module_2_api/lesson_1.py
+++ module_2_api/lesson_1.py
@@ -1,12 +1,18 @@
 import requests
 
-payload1 = {'T': '', 2: '', 'n': ''}
-print(requests.get(f"https://wttr.in/Сан-Франциско").text,
-      requests.get(f"https://wttr.in/Сан-Франциско", params=payload1).text)
 
-locations = ["Череповец", "svo", "Лондон"]
-payload2 = {'M': '', 'n': '', 'q': '', 'T': '', 'lang': 'ru'}
-for loc in locations:
-    response = requests.get(f"https://wttr.in/{loc}", params=payload2)
-    print(response.text)
-    response.raise_for_status()
+def main():
+    payload1 = {'main_actor': '1', 'is_published': 'True'}
+    print(requests.get(f"https://wttr.in/Сан-Франциско").text,
+          requests.get(f"https://wttr.in/Сан-Франциско", params=payload1).text)
+
+    locations = ["Череповец", "svo", "Лондон"]
+    payload2 = {'M': '', 'n': '', 'q': '', 'T': '', 'lang': 'ru'}
+    for loc in locations:
+        response = requests.get(f"https://wttr.in/{loc}", params=payload2)
+        print(response.text)
+        response.raise_for_status()
+
+
+if __name__ == "__main__":
+    main()
